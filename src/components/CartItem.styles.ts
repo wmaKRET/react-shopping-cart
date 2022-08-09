4ce0130edf68d8b8975f4278d53d4489be523ad0
wmaKRET <wkret91.2@gmail.com>
2022-08-09 10:06:06 +0200

    Add styles to CartItem component

--- src/components/CartItem.styles.ts
+++ src/components/CartItem.styles.ts
@@ -1,3 +1,25 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  padding-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  font-family: Arial, Helvetica, sans-serif;
+  border-bottom: 1px solid lightblue;
+
+  div {
+    flex: 1;
+  }
+
+  .information,
+  .buttons {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  img {
+    margin-left: 40px;
+    max-width: 80px;
+    object-fit: cover;
+  }
+`;
