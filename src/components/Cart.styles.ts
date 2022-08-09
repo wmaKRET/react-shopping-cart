d64867baa720fedd524bd66ff3a462fd58d1b601
wmaKRET <wkret91.2@gmail.com>
2022-08-09 09:39:42 +0200

    Create Cart and CartItem components - basic structure

---
+++ src/components/Cart.styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.aside`
+  width: 500px;
+  padding: 20px;
+  font-family: Arial, Helvetica, sans-serif;
+`;
---
+++ src/components/CartItem.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import { CartItemType } from "../App";
+import { Wrapper } from "./CartItem.styles";
+
+const CartItem: React.FC = () => <div>Cart Item</div>;
+
+export default CartItem;
